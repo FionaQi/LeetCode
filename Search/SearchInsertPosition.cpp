c9410a3a8b9e1e8bc3deaf696d4a6d9c3586b567
FionaQi <v-fenq@microsoft.com>
2015-09-12 23:56:02 +0800

    while terminate condition: when start == end, both return start

---
+++ Search/SearchInsertPosition.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int searchInsert(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size();
+		if (end < 0)
+			return -1;
+       
+        while( start <= end ){
+            const int mid = start + ( end - start ) >> 1;
+            if( nums[mid] == target )      
+                return mid;
+			else if( nums[mid] > target ){
+                end = mid - 1;
+            }
+            else if( nums[mid] < target){
+                start = mid + 1;
+            }
+        }
+		return start;
+    }
+
+int main()
+{
+	int A[] = {1, 3};
+	vector<int> a(A, A + 2);
+	int t = searchInsert(a, 2);
+	return 0;
+}
