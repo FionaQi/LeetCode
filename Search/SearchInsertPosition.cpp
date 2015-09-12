ea212b6f392da22c5ff05850b1061492efa81b18
FionaQi <v-fenq@microsoft.com>
2015-09-13 00:47:31 +0800

    tab

--- Search/SearchInsertPosition.cpp
+++ Search/SearchInsertPosition.cpp
@@ -2,24 +2,24 @@
 #include <vector>
 using namespace std;
 int searchInsert(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;   //notice
+    int start = 0;
+    int end = nums.size() - 1;   //notice
 	if (end < 0)
 		return -1;
        
-        while( start <= end ){     //note for "=", while terminate condition
-            const int mid = start + ( ( end - start ) >> 1);  // priority : + > >>
-            if( nums[mid] == target )      
-                return mid;
-			else if( nums[mid] > target ){
-                end = mid - 1;
-            }
-            else if( nums[mid] < target){
-                start = mid + 1;
-            }
+    while( start <= end ){     //note for "=", while terminate condition
+        const int mid = start + ( ( end - start ) >> 1);  // priority : + > >>
+        if( nums[mid] == target )      
+            return mid;
+		else if( nums[mid] > target ){
+            end = mid - 1;
         }
-	return start;             // both returns start
+        else if( nums[mid] < target){
+            start = mid + 1;
+        }
     }
+	return start;             // both returns start
+}
 
 int main()
 {
