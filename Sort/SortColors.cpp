fccb8f6c75994e88703ef1008c6e55e4e4b6bba7
FionaQi <v-fenq@microsoft.com>
2015-09-13 10:19:24 +0800

    sort color, o(n), 2 ptr

--- Sort/SortColors.cpp
+++ Sort/SortColors.cpp
@@ -2,6 +2,31 @@
 #include <vector>
 using namespace std;
 
+// o(n), with 2 ptr
+void sortColors2(vector<int> &nums) { 
+	int first = -1;
+	int second = nums.size();
+	if (second == 0)
+		return;
+	int cur = 0;
+	while (cur < second) {
+		if (nums[cur] < 1) {
+			first++;
+			swap(nums[first], nums[cur]);
+			cur++;
+		}
+		else if(nums[cur] > 1) {
+			second--;
+			swap(nums[second], nums[cur]);
+		}
+		else {
+			cur++;
+		}
+
+	}
+}
+
+// Quick sort
 void partition(vector<int> &nums, int pivot) {
         int left = 0; 
         int right = nums.size()-1;
@@ -13,11 +38,13 @@ void partition(vector<int> &nums, int pivot) {
             swap( nums[left], nums[right] );
         }
     }
+
 void swap(int &x, int &y){
         int temp = x;
         x = y;
         y = temp;
     }
+
 void sortColors(vector<int> &nums) {
         if( nums.size() <= 1 )
             return;
@@ -26,10 +53,8 @@ void sortColors(vector<int> &nums) {
     }
 
 void main(){
-
-	vector<int> num;
-	num.push_back(1);
-	num.push_back(0);
-	sortColors(num);
+	int a[] = {1, 0, 1,2,0,1,1};
+	vector<int> num(a, a+ 7);
+	sortColors2(num);
 	int t;
 	}
