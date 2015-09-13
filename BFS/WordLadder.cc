b8fdfb707d3ac0b38876cbf6b3ce110177550508
FionaQi <v-fenq@microsoft.com>
2015-09-13 10:52:59 +0800

    use two queues to store curent level broad search result and last level broad search result

--- BFS/WordLadder.cc
+++ BFS/WordLadder.cc
@@ -8,56 +8,56 @@ using namespace std;
 class Solution {
 public:
     int ladderLength(string start, string end, unordered_set<string>& dict) {
-        if(start == end)    return 2;
-        if(dict.size() <= 0)    return 0;
-		int len = start.size();
-        queue<string> que;
-        unordered_set<string> visited;
-        int res = 1;
-        que.push(start);
-        que.push("");
-        while(!que.empty()) {
-            string cur = que.front();
-            que.pop();
-            if(cur == "") {
-                if(que.empty()) {
-                    return 0;
-                } else {
-                    res++;
-                    que.push("");
-                    continue;
-                }
-            }
-            for(int i = 0; i < len; i++) {
-                char temp = cur[i];
-                for(char c= 'a'; c <= 'z'; c++) {
-                    if(c == temp)   continue;
-                    cur[i] = c;
-                    if(cur == end)  return res+1;
-                    if(dict.find(cur) != dict.end()) {
-                        if(visited.find(cur) == visited.end()) {
-                            que.push(cur);
-                            visited.insert(cur);
-                        }
-                    }
-                }
-                cur[i] = temp;
-            }
-        }
+        if(start == end)    return 1;
+        //if(dict.size() <= 0)    return 0;
+		this->shortestLen = 1;
+        lastLevel.push(start);
+		return bfs(end, dict);
     }
+private:
+	queue<string> curLevel;
+	queue<string> lastLevel;
+	unordered_set<string> visited;
+	int shortestLen;
+	int bfs(string end, unordered_set<string>& dict){
+		while (!lastLevel.empty()) {
+			string cur = lastLevel.front();
+			lastLevel.pop();
+			int len = end.size();
+			for (int k = 0; k < len; k++) {
+				const char temp = cur[k];
+				for (char c = 'a'; c <= 'z'; c++) {
+					if (c == temp)   continue;   //check
+					cur[k] = c; 
+					if (cur == end)  return this->shortestLen + 1;
+					if (dict.find(cur) != dict.end()) {
+						if (visited.find(cur) == visited.end()) {
+							curLevel.push(cur);
+							visited.insert(cur);
+						}
+					}
+				}
+				cur[k] = temp;
+			}
+			if (lastLevel.empty()) {     //两个条件相反
+				swap(lastLevel, curLevel);
+				this->shortestLen++;
+			}
+		}
+		return 0;
+	}
 };
 
 int main() {
-   // int t[] = { 5, 2, 1, 2, 1, 5 };
-	
-	//vector<string> r(t, t + sizeof(t) / sizeof(int));
-	string a[] = {"si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"};
-	unordered_set<string> dict(a, a + 95 );
-	//dict.insert("hot");
-	//dict.insert("dog");
-	//dict.insert("dot");
+//	string a[] = {"si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"};
+//	unordered_set<string> dict(a, a + 95 );
+	unordered_set<string> dict;
+	dict.insert("hot");
+	dict.insert("dog");
+	dict.insert("dot");
 	Solution s;
-	int res = s.ladderLength("qa", "sq", dict);
+	int res = s.ladderLength("hot", "dog", dict);
+	//int res = s.ladderLength("qa", "sq", dict);
 	cout << res << endl;
 	return 0;
 }
