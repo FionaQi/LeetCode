79bb63b4446da282589ea0dda3bf6e91552ee711
FionaQi <v-fenq@microsoft.com>
2015-09-14 15:44:08 +0800

    reduce o(m) space

--- Sort/MergeSortedArray.cc
+++ Sort/MergeSortedArray.cc
@@ -19,7 +19,28 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
             nums1[i] = nums2[q++];
         }
     }
-    
+}
+
+void merge2(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+	if (n <= 0)	return;
+	if (m <= 0) {
+		nums1 = nums2;
+		return;
+	}
+	int p1 = m - 1;
+	int p2 = n - 1;
+	int z = m + n - 1;
+	while (p1 >= 0 && p2 >= 0) {
+		if (nums2[p2] > nums1[p1]) {
+			nums1[z--] = nums2[p2--];
+		}
+		else {
+			nums1[z--] = nums1[p1--];
+		}
+	}
+	while (p2 >= 0) {
+		nums1[z--] = nums2[p2--];
+	}
 }
 
 int main() {
